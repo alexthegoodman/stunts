f5fb854f2cf3b6d89f8f9524b32f4910900a9c49
Alex Goodman <alexthegoodman@gmail.com>
2024-10-15 12:58:51 -0400

    working usage of floem dropdown

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 use floem::peniko::Color;
+use floem::reactive::{SignalGet, SignalUpdate};
+use floem::views::text;
 use floem::views::{h_stack, svg, v_stack};
 use floem::{
     reactive::create_signal,
     unit::UnitExt, // Import UnitExt for .px() method
-    views::{button, label, Decorators},
+    views::{button, dropdown, label, Decorators},
     IntoView,
 };
 
+// Define an enum for our dropdown options
+#[derive(Clone, PartialEq, Debug)]
+enum DropdownOption {
+    Option1,
+    Option2,
+    Option3,
+}
+
+impl std::fmt::Display for DropdownOption {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DropdownOption::Option1 => write!(f, "Option 1"),
+            DropdownOption::Option2 => write!(f, "Option 2"),
+            DropdownOption::Option3 => write!(f, "Option 3"),
+        }
+    }
+}
+
 fn app_view() -> impl IntoView {
     let (counter, mut set_counter) = create_signal(0);
+    let (selected_option, set_selected_option) = create_signal(DropdownOption::Option1);
+
+    println!("selected_option {:?}", selected_option.get());
 
     (
         label(move || format!("Value: {counter}")).style(|s| s.margin_bottom(10)),
@@ -17,6 +40,32 @@ fn app_view() -> impl IntoView {
             styled_button("Decrement", "minus", move || set_counter -= 1),
         )
             .style(|s| s.flex_col().gap(10).margin_top(10)),
+        dropdown::dropdown(
+            // Active item (currently selected option)
+            move || {
+                let see = selected_option.get();
+                println!("see {:?}", see);
+                see
+            },
+            // Main view (what's always visible)
+            |option: DropdownOption| Box::new(label(move || format!("Selected: {}", option))),
+            // Iterator of all options
+            vec![
+                DropdownOption::Option1,
+                DropdownOption::Option2,
+                DropdownOption::Option3,
+            ],
+            // List item view (how each option in the dropdown is displayed)
+            // move |option: DropdownOption| {
+            //     let option_clone = option.clone();
+            //     Box::new(button(option.to_string()).action(move || {
+            //         println!("DropdownOption {:?}", option_clone.clone());
+            //         set_selected_option.set(option_clone.clone());
+            //     }))
+            // },
+            move |m| text(m.to_string()).into_any(),
+        )
+        .on_accept(move |new| set_selected_option.set(new)),
     )
         .style(|s| s.flex_col().items_center())
 }
